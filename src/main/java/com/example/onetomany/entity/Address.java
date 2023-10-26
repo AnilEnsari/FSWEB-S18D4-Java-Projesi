ef30a77051eccb56ff96fa66b3b644cb5be221df
AnilEnsari <anilensari@gmail.com>
2023-10-26 23:11:02 +0300

    mission 1 is done

--- src/main/java/com/example/onetomany/entity/Address.java
+++ src/main/java/com/example/onetomany/entity/Address.java
@@ -1,12 +1,10 @@
 package com.example.onetomany.entity;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
-@AllArgsConstructor
 @NoArgsConstructor
 @Entity
 @Table (name = "address", schema = "fsweb")
@@ -14,17 +12,18 @@ public class Address {
 
     @Id
     @GeneratedValue (strategy = GenerationType.IDENTITY)
-  @Column(name = "address_id")
-    private int  adressId ;
-    @Column(name = "street_no")
-    private int streetNo ;
+  @Column(name = "id")
+    private int  id ;
+    @Column(name = "street")
+    private String street ;
+    @Column(name = "no")
+    private int no ;
     @Column(name = "city")
     private String city;
     @Column(name = "country")
     private String country ;
     @Column(name = "description")
     private String description ;
-    @OneToOne(cascade = {CascadeType.MERGE,CascadeType.DETACH,CascadeType.PERSIST,CascadeType.REFRESH},
-    mappedBy = "address")
+    @OneToOne(mappedBy = "address",cascade = {CascadeType.MERGE,CascadeType.DETACH,CascadeType.PERSIST,CascadeType.REFRESH})
     private Customer customer ;
 }
--- src/main/java/com/example/onetomany/entity/Customer.java
+++ src/main/java/com/example/onetomany/entity/Customer.java
@@ -2,7 +2,6 @@ package com.example.onetomany.entity;
 
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -10,26 +9,26 @@ import java.util.List;
 
 @Data
 @NoArgsConstructor
-@AllArgsConstructor
 @Entity
 @Table(name = "customer", schema = "fsweb")
 public class Customer {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "customer_id")
-    private int customerId ;
+    @Column(name = "id")
+    private int id ;
   @Column(name = "first_name")
     private String firstName;
   @Column (name = "last_name")
    private String lastName;
    @Column(name = "email")
     private String email;
+   @Column(name = "salary")
+   private Double salary ;
    @OneToOne(cascade = CascadeType.ALL)
    @JoinColumn(name = "address_id")
    private Address address ;
 
-   @OneToMany (mappedBy = "customer", cascade = {CascadeType.DETACH, CascadeType.MERGE,
-           CascadeType.PERSIST, CascadeType.REFRESH})
-   private List<Account> accounts ;
+   @OneToMany (mappedBy = "customer", cascade = CascadeType.ALL)
+   private List<Account> accountList ;
 
 }
